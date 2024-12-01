03d0df0d2f0c1efcf16b9af2dbb6f9d40676dcc0
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-01 13:21:36 +0000

    Day 1 part 2

--- src/days/day1.cpp
+++ src/days/day1.cpp
@@ -1,25 +1,37 @@
 #include "aoc_days.h"
 
+#include <map>
+
 void aoc::day1(std::string_view inputFilePath)
 {
     std::ifstream file(inputFilePath.data());
-
-    std::vector<int> left, right;
+    std::vector<int> leftEntries, rightEntries;
+    std::map<int, int> rightEntryCounts;
 
     int a, b;
     while (file >> a >> b)
     {
-        left.insert(std::lower_bound(left.begin(), left.end(), a, std::less<int>()), a);
-        right.insert(std::lower_bound(right.begin(), right.end(), b, std::less<int>()), b);
+        leftEntries.insert(std::lower_bound(leftEntries.begin(), leftEntries.end(), a, std::less<int>()), a);
+        rightEntries.insert(std::lower_bound(rightEntries.begin(), rightEntries.end(), b, std::less<int>()), b);
+
+        if (!rightEntryCounts.try_emplace(b, 1).second)
+            rightEntryCounts[b] += 1;
     }
 
-    int diffTotal = 0;
-    for (int i = 0; i < left.size(); ++i)
+    size_t diffTotal = 0;
+    size_t similarityScore = 0;
+    for (int i = 0; i < leftEntries.size(); ++i)
     {
-        const int diff = abs(left[i] - right[i]);
+        const int left = leftEntries[i];
+        const int right = rightEntries[i];
+
+        const int diff = abs(left - right);
         diffTotal += diff;
+
+        const auto rightCountEntry = rightEntryCounts.find(left);
+        similarityScore += (rightCountEntry != rightEntryCounts.end()) ? (left * rightCountEntry->second) : 0;
     }
 
-
-    std::print("Total difference {}", diffTotal);
+    std::print("Total difference: {}\n", diffTotal);
+    std::print("Total similarity score: {}\n", similarityScore);
 }
