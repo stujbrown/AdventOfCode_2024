0ca535b942b9bf78551bb9876abb971b20cea69d
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-01 13:01:15 +0000

    Day 1

--- src/days/day1.cpp
+++ src/days/day1.cpp
@@ -2,5 +2,24 @@
 
 void aoc::day1(std::string_view inputFilePath)
 {
-	(void)inputFilePath;
+    std::ifstream file(inputFilePath.data());
+
+    std::vector<int> left, right;
+
+    int a, b;
+    while (file >> a >> b)
+    {
+        left.insert(std::lower_bound(left.begin(), left.end(), a, std::less<int>()), a);
+        right.insert(std::lower_bound(right.begin(), right.end(), b, std::less<int>()), b);
+    }
+
+    int diffTotal = 0;
+    for (int i = 0; i < left.size(); ++i)
+    {
+        const int diff = abs(left[i] - right[i]);
+        diffTotal += diff;
+    }
+
+
+    std::print("Total difference {}", diffTotal);
 }
