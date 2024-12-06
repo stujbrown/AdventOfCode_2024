a1d959d76e75e2591d6e20f8545454e79c429315
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-06 13:00:03 +0000

    Day 6 part 2

--- src/days/day6.cpp
+++ src/days/day6.cpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <set>
+#include <optional>
 
 namespace
 {
@@ -11,6 +12,49 @@ namespace
         Coord operator+(const Coord& rhs) { return Coord{ x + rhs.x, y + rhs.y }; };
     };
     bool operator<(const Coord& lhs, const Coord& rhs) { return lhs.x < rhs.x || (lhs.x == rhs.x && lhs.y < rhs.y); };
+    bool operator==(const Coord& lhs, const Coord& rhs) { return lhs.x == rhs.x && lhs.y == rhs.y; };
+
+    bool isInBounds(const std::vector<std::string>& grid, const Coord& c) { return c.x >= 0 && c.x < grid[0].length() && c.y >= 0 && c.y < grid.size(); };
+
+    std::optional<std::pair<size_t, size_t>> simulate(const std::vector<std::string>& grid, const Coord& guardStartPos, Coord guardFacing, const std::optional<Coord>& blocker = std::nullopt)
+    {
+        Coord guardPos = guardStartPos;
+
+        std::set<Coord> visitedPositions;
+        std::set<Coord> successfulBlockerPositions;
+        std::set<std::pair<Coord, Coord>> visitedPositionsWithDirections;
+        for (; isInBounds(grid, guardPos);)
+        {
+            visitedPositions.emplace(guardPos);
+
+            const Coord posInFront = guardPos + guardFacing;
+            if (isInBounds(grid, posInFront) && (grid[posInFront.y][posInFront.x] == '#' || (blocker.has_value() && posInFront == blocker)))
+            {
+                guardFacing = { -guardFacing.y, guardFacing.x }; // Rotate vector by 90 degrees
+            }
+            else // Rotate or move, not both as the guard can turn into a new obstacle
+            {
+                if (!blocker.has_value() && isInBounds(grid, posInFront) && !successfulBlockerPositions.contains(posInFront) && posInFront != guardStartPos)
+                {
+                    // Start a sub-simulation if not already in one
+                    if (!simulate(grid, guardStartPos, Coord{ 0, -1 }, posInFront).has_value()) // Place blocker immediately in-front of the guard to cause a deviation
+                    {
+                        successfulBlockerPositions.emplace(posInFront);
+                    }
+                }
+
+                guardPos = posInFront;
+
+                const auto guardPosAndFacing = std::make_pair(guardPos, guardFacing);
+                if (visitedPositionsWithDirections.contains(guardPosAndFacing))
+                    return std::nullopt;
+
+                visitedPositionsWithDirections.emplace(guardPosAndFacing);
+            }
+        }
+
+        return std::make_pair(visitedPositions.size(), successfulBlockerPositions.size());
+    }
 }
 
 void aoc::day6(std::string_view inputFilePath)
@@ -20,7 +64,6 @@ void aoc::day6(std::string_view inputFilePath)
     // 0 = up, 1 = right, 2 = down, 3 = left.
     Coord guardPos = { -1, -1 };
     Coord guardFacing = { 0, -1 };  // Guard always faces up to start.
-    std::set<Coord> visitedPositions;
 
     std::vector<std::string> grid;
     for (std::string line; std::getline(file, line);)
@@ -32,22 +75,8 @@ void aoc::day6(std::string_view inputFilePath)
         }
     }
 
-    auto isInBounds = [&grid](const Coord& c) { return c.x >= 0 && c.x < grid[0].length() && c.y >= 0 && c.y < grid.size(); };
+    const std::optional<std::pair<size_t, size_t>> visitedPositions = simulate(grid, guardPos, guardFacing);
 
-    for (; isInBounds(guardPos);)
-    {
-        visitedPositions.emplace(guardPos);
-
-        const Coord posInFront = guardPos + guardFacing;
-        if (isInBounds(posInFront) && grid[posInFront.y][posInFront.x] == '#')
-        {
-            guardFacing = { -guardFacing.y, guardFacing.x }; // Rotate vector by 90 degrees
-        }
-        else // Rotate or move, not both as the guard can turn into a new obstacle
-        {
-            guardPos = posInFront;
-        }
-    }
-
-    std::println("Number of visited positions: {}", visitedPositions.size());
+    std::println("Number of visited positions: {}", visitedPositions->first);
+    std::println("Number of possible blocker positions: {}", visitedPositions->second);
 }
