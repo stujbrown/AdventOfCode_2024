480e1a918376b8f337463695171fc2dab71e2040
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-09 17:53:21 +0000

    Day 9 tidy

--- src/days/day9.cpp
+++ src/days/day9.cpp
@@ -5,34 +5,36 @@
 
 namespace
 {
+    std::span<int> findSpace(std::vector<int>& diskLayout, size_t spaceRequired, int& searchStartIndex, int searchStopIndex)
+    {
+        for (int i = 0, l1 = searchStartIndex, l2 = l1; l2 < searchStopIndex; l1 = l2 + 1, ++i)
+        {
+            while (l1 < diskLayout.size() && diskLayout[l1] != -1) ++l1;
+            l2 = l1;
+            while (l2 < diskLayout.size() - 1 && diskLayout[l2 + 1] == -1) ++l2;
+            if (i == 0)
+                searchStartIndex = l1;
+
+            if ((l2 - l1) + 1 >= spaceRequired && (l2 < searchStopIndex) && l2 < diskLayout.size())
+                return std::span<int>(diskLayout.begin() + l1, diskLayout.begin() + l1 + spaceRequired);
+        }
+        return {};
+    }
+
     size_t compact(std::vector<int>&& diskLayout, bool allowFragmentation)
     {
         int lStart = 0;
-        size_t lastId = std::numeric_limits<size_t>::max();
         for (int r1 = -1, r2 = (int)diskLayout.size() - 1; r2 >= 0; r2 = r1 - 1)
         {
             while (r2 >= 0 && diskLayout[r2] == -1) --r2;
             r1 = r2;
-            while ((r1 > 0 && diskLayout[r1 - 1] == diskLayout[r2]) && !allowFragmentation) --r1;
+            while (!allowFragmentation && (r1 > 0 && diskLayout[r1 - 1] == diskLayout[r2])) --r1;
             const size_t spaceRequired = (r2 - r1) + 1;
 
-            if (diskLayout[r2] > lastId) continue;
-            lastId = diskLayout[r2];
-
-            for (int i = 0, l1 = lStart, l2 = lStart; (l2 < diskLayout.size()) && (l2 < r1); l1 = l2 + 1, ++i)
+            if (std::span<int> space = findSpace(diskLayout, spaceRequired, lStart, r1); !space.empty())
             {
-                while (l1 < diskLayout.size() && diskLayout[l1] != -1) ++l1;
-                l2 = l1;
-                while (l2 < diskLayout.size() - 1 && diskLayout[l2 + 1] == -1) ++l2;
-                if (i == 0)
-                    lStart = l1;
-
-                if ((l2 - l1) + 1 >= spaceRequired && (l2 < r1) && l2 < diskLayout.size())
-                {
-                    std::fill(diskLayout.begin() + l1, diskLayout.begin() + (l1 + spaceRequired), diskLayout[r1]);
-                    std::fill(diskLayout.begin() + r1, diskLayout.begin() + (r1 + spaceRequired), -1);
-                    break;
-                }
+                std::fill(space.begin(), space.end(), diskLayout[r1]);
+                std::fill(diskLayout.begin() + r1, diskLayout.begin() + (r1 + spaceRequired), -1);
             }
         }
 
@@ -55,6 +57,6 @@ void aoc::day9(std::string_view inputFilePath)
         diskLayout.append_range(std::views::repeat(-1, (file >> c) ? int(c - '0') : 0));
     }
 
-    std::println("Compacted checksum: {}", compact(std::vector<int>(diskLayout), true));
-    std::println("Optimal checksum: {}", compact(std::vector<int>(diskLayout), false));
+    std::println("Compacted checksum: {}", compact(std::move(std::vector<int>(diskLayout)), true));
+    std::println("Optimal checksum: {}", compact(std::move(std::vector<int>(diskLayout)), false));
 }
