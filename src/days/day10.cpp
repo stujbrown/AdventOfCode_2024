7e9c56379d8868b03937a9256c67ac5e6606e18b
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-10 18:07:00 +0000

    Day 10 tidy

--- src/days/day10.cpp
+++ src/days/day10.cpp
@@ -4,29 +4,27 @@
 #include <vector>
 #include <set>
 
-namespace
+struct HoofItSolver
 {
-    struct TrailheadStats { size_t score, rating; };
-    void findNextSteps(const std::vector<std::string>& map, std::set<Vec2>& reachedTrailheads, Vec2 pos, TrailheadStats& stats)
+    size_t score = 0, rating = 0;
+
+    void solve(const std::vector<std::string>& map, std::set<Vec2>& reachedTrailheads, Vec2 pos)
     {
-        std::pair<size_t, size_t> results = {};
         if (map[pos.y][pos.x] == '9')
         {
-            stats.score += (!reachedTrailheads.contains(pos) ? 1 : 0);
-            stats.rating += 1;
-            reachedTrailheads.emplace(pos);
+            score += reachedTrailheads.insert(pos).second ? 1 : 0;
+            rating += 1;
+            return;
         }
-        else
+
+        for (const Vec2& direction : std::initializer_list<Vec2>{ {1, 0}, {-1, 0}, {0, 1}, {0, -1} })
         {
-            for (const Vec2& direction : std::initializer_list<Vec2>{ {1, 0}, {-1, 0},{0, 1},{0, -1} })\
-            {
-                const Vec2 newPos = { pos.x + direction.x, pos.y + direction.y };
-                if ((newPos.x >= 0 && newPos.x < map[0].length() && newPos.y >= 0 && newPos.y < map.size()) && map[newPos.y][newPos.x] == map[pos.y][pos.x] + 1)
-                    findNextSteps(map, reachedTrailheads, Vec2{ newPos.x, newPos.y }, stats);
-            }
+            const Vec2 newPos = { pos.x + direction.x, pos.y + direction.y };
+            if ((newPos.x >= 0 && newPos.x < map[0].length() && newPos.y >= 0 && newPos.y < map.size()) && map[newPos.y][newPos.x] == map[pos.y][pos.x] + 1)
+                solve(map, reachedTrailheads, Vec2{ newPos.x, newPos.y });
         }
     }
-}
+};
 
 void aoc::day10(std::string_view inputFilePath)
 {
@@ -35,7 +33,7 @@ void aoc::day10(std::string_view inputFilePath)
     for (std::string line; std::getline(file, line);)
         map.emplace_back(line);
 
-    TrailheadStats trailheadStats = {};
+    HoofItSolver solver;
     for (int y = 0; y < map.size(); ++y)
     {
         for (int x = 0; x < map[0].length(); ++x)
@@ -43,10 +41,10 @@ void aoc::day10(std::string_view inputFilePath)
             if (map[y][x] == '0')
             {
                 std::set<Vec2> reachedTrailheads;
-                findNextSteps(map, reachedTrailheads, Vec2{ x, y }, trailheadStats);
+                solver.solve(map, reachedTrailheads, Vec2{ x, y });
             }
         }
     }
-    std::println("Trailhead score: {}", trailheadStats.score);
-    std::println("Trailhead rating: {}", trailheadStats.rating);
+    std::println("Trailhead score: {}", solver.score);
+    std::println("Trailhead rating: {}", solver.rating);
 }
