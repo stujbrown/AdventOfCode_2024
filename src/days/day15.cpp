2252bb36fc033af19440a57e3ff3a120da1f59b9
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-16 10:56:56 +0000

    Day 16 part 2

--- src/days/day15.cpp
+++ src/days/day15.cpp
@@ -2,23 +2,17 @@
 #include "vec2.h"
 #include <vector>
 
-void aoc::day15(std::string_view inputFilePath)
+namespace
 {
-    std::vector<std::string> map;
-    Vec2 robotPos{};
-    std::ifstream file(inputFilePath.data());
-    for (std::string line; std::getline(file, line) && line[0] == '#';)
+    void solve(std::vector<std::string>& map, const std::vector<char>& instructions)
     {
-        map.emplace_back(line);
-        if (size_t pos = line.find('@'); pos != std::string::npos)
-        {
-            robotPos = { (int)pos, (int)map.size() - 1 };
-        }
-    }
+        Vec2 robotPos{};
+        for (size_t y = 0; y < map.size(); ++y)
+            for (size_t x = 0; x < map[0].length(); ++x)
+                if (map[y][x] == '@')
+                    robotPos = { (int)x, (int)y };
 
-    for (std::string line; std::getline(file, line);)
-    {
-        for (char c : line)
+        for (char c : instructions)
         {
             Vec2 dir{};
             if (c == '<') dir = { -1, 0 };
@@ -27,14 +21,46 @@ void aoc::day15(std::string_view inputFilePath)
             else if (c == 'v') dir = { 0, 1 };
 
             const Vec2 next = robotPos + dir;
-            if (map[next.y][next.x] == 'O')
+            if (map[next.y][next.x] == 'O' || map[next.y][next.x] == '[' || map[next.y][next.x] == ']')
             {
-                Vec2 pushNext;
-                for (pushNext = next + dir; map[pushNext.y][pushNext.x] == 'O'; pushNext += dir);
-                if (map[pushNext.y][pushNext.x] != '#')
+                std::vector<Vec2> affectedBoxCells, pushCheckQueue;
+                pushCheckQueue.push_back(next);
+                bool canPush = true;
+                while (!pushCheckQueue.empty())
                 {
-                    map[pushNext.y][pushNext.x] = 'O';
-                    map[next.y][next.x] = '.';
+                    const auto pushNext = pushCheckQueue.front();
+                    const char nextValue = map[pushNext.y][pushNext.x];
+                    pushCheckQueue.erase(pushCheckQueue.begin());
+
+                    if (std::find(affectedBoxCells.begin(), affectedBoxCells.end(), pushNext) != affectedBoxCells.end() ||
+                        nextValue == '.')
+                        continue;
+
+                    if (nextValue == '#')
+                    {
+                        canPush = false;
+                        break;
+                    }
+
+                    affectedBoxCells.emplace_back(pushNext);
+
+                    if (nextValue == 'O' || nextValue == '[' || nextValue == ']')
+                    {
+                        pushCheckQueue.emplace_back(pushNext + dir);
+                        if (nextValue == '[' && dir.y != 0)
+                            pushCheckQueue.emplace_back(pushNext + Vec2{ 1, 0 });
+                        else if (nextValue == ']' && dir.y != 0)
+                            pushCheckQueue.emplace_back(pushNext + Vec2{ -1, 0 });
+                    }
+                }
+
+                if (canPush)
+                {
+                    for (auto itr = affectedBoxCells.rbegin(); itr != affectedBoxCells.rend(); ++itr)
+                    {
+                        map[itr->y + dir.y][itr->x + dir.x] = map[itr->y][itr->x];
+                        map[itr->y][itr->x] = '.';
+                    }
                 }
             }
 
@@ -45,13 +71,43 @@ void aoc::day15(std::string_view inputFilePath)
                 robotPos = next;
             }
         }
+    }
+}
 
+
+void aoc::day15(std::string_view inputFilePath)
+{
+    std::vector<std::string> map, bigMap;
+    std::vector<char> instructions;
+    std::ifstream file(inputFilePath.data());
+    for (std::string line; std::getline(file, line) && line[0] == '#';)
+    {
+        map.emplace_back(line);
+        bigMap.push_back({});
+        for (char c : line)
+        {
+            if (c == 'O') bigMap.back().append("[]");
+            else if (c == '#') bigMap.back().append("##");
+            else if (c == '@') bigMap.back().append("@.");
+            else bigMap.back().append("..");
+        }
     }
 
-    size_t boxGpsSum = 0;
-    for (size_t y = 0; y < map.size(); ++y)
-        for (size_t x = 0; x < map[0].length(); ++x)
-            if (map[y][x] == 'O') boxGpsSum += 100 * y + x;
+    for (std::string line; std::getline(file, line);)
+        for (char c : line)
+            instructions.emplace_back(c);
 
-    std::println("Box GPS sum: {}", boxGpsSum);
+    solve(map, instructions);
+    solve(bigMap, instructions);
+
+    auto sum = [](const std::vector<std::string >& map) {
+        size_t boxGpsSum = 0;
+        for (size_t y = 0; y < map.size(); ++y)
+            for (size_t x = 0; x < map[0].length(); ++x)
+                if (map[y][x] == 'O' || map[y][x] == '[') boxGpsSum += 100 * y + x;
+        return boxGpsSum;
+        };
+
+    std::println("Box GPS sum: {}", sum(map));
+    std::println("Box GPS sum for larger warehouse: {}", sum(bigMap));
 }
