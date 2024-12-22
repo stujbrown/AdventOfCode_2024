e6f466f0578c434c208fd31d26b70b1d6418a4fd
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-22 12:25:49 +0000

    Day 22 part 1

--- src/days/day22.cpp
+++ src/days/day22.cpp
@@ -1,6 +1,37 @@
 #include "aoc_days.h"
 
+namespace
+{
+    int64_t solve(int64_t number)
+    {
+        for (int64_t i = 0; i < 2000; ++i)
+        {
+            int64_t operand = number * 64;
+            number = number ^ operand;
+            number = number % 16777216;
+
+
+            operand = number / 32;
+            number = number ^ operand;
+            number = number % 16777216;
+
+
+            operand = number * 2048;
+            number = number ^ operand;
+            number = number % 16777216;
+        }
+        return number;
+    }
+}
+
 void aoc::day22(std::string_view inputFilePath)
 {
-	(void)inputFilePath;
+    size_t secretSum = 0;
+    std::ifstream file(inputFilePath.data());
+    for (int64_t secretNumber = 0; file >> secretNumber;)
+    {
+        secretSum += (size_t)solve(secretNumber);
+    }
+
+    std::println("Secret sum: {}", secretSum);
 }
