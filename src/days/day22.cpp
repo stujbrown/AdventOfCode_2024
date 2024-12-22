45d7b75a05c9751378df3dec68bc3ccf16b8ccba
Stuart Brown <stuart.j.brown.90@gmail.com>
2024-12-22 14:17:15 +0000

    Day 22 part 2

--- src/days/day22.cpp
+++ src/days/day22.cpp
@@ -1,72 +1,39 @@
 #include "aoc_days.h"
 #include <map>
 #include <array>
-#include <vector>
 #include <set>
 
 namespace
 {
-    int64_t solve(int64_t number)
+    int64_t solve(int64_t number, std::map<uint32_t, size_t>& sequenceRecords)
     {
+        std::array<int16_t, 4> changeHistory;
+        std::set<uint32_t> visitedThisTrader;
+
+        int16_t previousPrice = -1;
         for (int64_t i = 0; i < 2000; ++i)
         {
-            int64_t operand = number * 64;
-            number = number ^ operand;
-            number = number % 16777216;
+            number = (number ^ (number * 64)) % 16777216;
+            number = (number ^ (number / 32)) % 16777216;
+            number = (number ^ (number * 2048)) % 16777216;
 
-
-            operand = number / 32;
-            number = number ^ operand;
-            number = number % 16777216;
-
-
-            operand = number * 2048;
-            number = number ^ operand;
-            number = number % 16777216;
-        }
-        return number;
-    }
-
-    int64_t solve2(int64_t number, std::map<std::array<int, 4>, size_t>& sequenceRecords)
-    {
-        std::array<int, 4> changeHistory;
-        std::set<std::array<int, 4>> visitedThisTrader;
-
-        int previousPrice = -1;
-        for (int64_t i = 0; i < 2000; ++i)
-        {
-            int64_t operand = number * 64;
-            number = number ^ operand;
-            number = number % 16777216;
-
-
-            operand = number / 32;
-            number = number ^ operand;
-            number = number % 16777216;
-
-
-            operand = number * 2048;
-            number = number ^ operand;
-            number = number % 16777216;
-
-
-            const int price = number % 10;
+            const int16_t price = number % 10;
             if (i != 0)
             {
-                const int change = price - previousPrice;
-                memmove(changeHistory.data(), changeHistory.data() + 1, sizeof(int) * 3);
+                const int16_t change = price - previousPrice;
+                memmove(changeHistory.data(), changeHistory.data() + 1, sizeof(int16_t) * 3);
                 changeHistory[3] = change;
                 if (i > 3)
                 {
-                    if (!visitedThisTrader.contains(changeHistory))
+                    const uint32_t changeHash = (uint32_t)((changeHistory[0] + 18) | ((changeHistory[1] + 18) << 5) | ((changeHistory[2] + 18) << (5 * 2)) | (changeHistory[3] + 18) << (5 * 3));
+                    if (!visitedThisTrader.contains(changeHash))
                     {
-                        auto recordItr = sequenceRecords.find(changeHistory);
+                        auto recordItr = sequenceRecords.find(changeHash);
                         if (recordItr == sequenceRecords.end())
-                            recordItr = sequenceRecords.emplace(std::make_pair(changeHistory, 0)).first;
+                            recordItr = sequenceRecords.emplace(std::make_pair(changeHash, 0)).first;
 
                         recordItr->second += price;
-
-                        visitedThisTrader.emplace(changeHistory);
+                        visitedThisTrader.emplace(changeHash);
                     }
                 }
             }
@@ -82,11 +49,10 @@ void aoc::day22(std::string_view inputFilePath)
     size_t secretSum = 0;
     std::ifstream file(inputFilePath.data());
 
-    std::map<std::array<int, 4>, size_t> sequenceRecords;
+    std::map<uint32_t, size_t> sequenceRecords;
     for (int64_t secretNumber = 0; file >> secretNumber;)
     {
-        secretSum += (size_t)solve(secretNumber);
-        solve2(secretNumber, sequenceRecords);
+        secretSum += (size_t)solve(secretNumber, sequenceRecords);
     }
 
     size_t highestTotal = 0;
